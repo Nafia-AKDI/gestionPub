304f694136887ec01cea7ccbb0ff7b709a9ca176
Nafia-AKDI <nafieakdi1@gmail.com>
2024-02-26 12:21:02 +0100

    v3

--- src/main/resources/db/migration/V0.2__alter_table.sql
+++ src/main/resources/db/migration/V0.2__alter_table.sql
@@ -3,11 +3,13 @@ ALTER TABLE annonces_cibles DROP CONSTRAINT annonces_cibles_pkey;
 
 -- Modifier les colonnes pour corriger les références
 ALTER TABLE annonces_cibles
-DROP COLUMN annonce_id,
-    DROP COLUMN cible_id;
+DROP
+COLUMN annonce_id,
+    DROP
+COLUMN cible_id;
 
 ALTER TABLE annonces_cibles
-    ADD COLUMN annonce_id INTEGER REFERENCES annonces(id),
+    ADD COLUMN annonce_id INTEGER REFERENCES annonces (id),
     ADD COLUMN cible_id INTEGER REFERENCES cibles(id);
 
 -- Ajouter une nouvelle contrainte de clé primaire
